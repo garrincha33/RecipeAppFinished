862f7de974d68f700ac2a003c879806dd9a1b98c
Richard Price <mrspondooli@gmail.com>
2016-02-07 19:12:26 +0000

    added core data entities and attributes

---
+++ Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  Recipiez
+//
+//  Created by Richard Price on 07/02/2016.
+//  Copyright © 2016 twisted echo. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Recipe: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
